8e47bab5cc91d754faba96e20ac856c179e50607
EdsonRego <edsonxrego@gmail.com>
2026-02-11 20:56:30 -0300

    Color Library Setup

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_banco_douro/ui/stiles/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,7 +8,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFFF0F0F0),
+        backgroundColor: AppColor.lightGrey,
         title: const Text("Sistema de Gestão de Contas"),
         actions: [
           IconButton(
